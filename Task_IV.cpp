7c5e4bd3f4728e712d627bb0557215218ab56cfc
uskawi <62665540+uskawi@users.noreply.github.com>
2022-06-28 01:53:43 +0100

    added openLog function

--- Task_IV.cpp
+++ Task_IV.cpp
@@ -13,3 +13,28 @@ struct berResults
     clock_t t1;
     clock_t t2;
 };
+
+std::fstream logFile;
+
+void saveLog(std::string msg)
+{
+    std::string ss;
+    time_t currentTime = time(NULL);
+    std::string txttime = (std::string)asctime(localtime(&currentTime));
+    txttime = txttime.substr(0, txttime.length() - 1);
+    ss = (std::string)"T: " + txttime + " M: " + msg + "\n";
+    logFile << ss.c_str();
+    logFile.flush();
+    std::cout << ss.c_str();
+    std::cout.flush();
+}
+
+void openLog(std::string fileName)
+{
+    logFile.open(fileName.c_str(), std::ios_base::app);
+    if (!logFile.good() == true)
+    {
+        std::cout << "Nie mozna otworzyc pliku log" << "log.txt" << std::endl;
+    }
+    saveLog("Plik log otwarty");
+}
