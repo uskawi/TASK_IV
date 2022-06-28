bdc257b8a519a9f7ec1399757b7e7ac2be08b29d
uskawi <62665540+uskawi@users.noreply.github.com>
2022-06-28 01:55:53 +0100

    added hammingDistance function

--- Task_IV.cpp
+++ Task_IV.cpp
@@ -44,3 +44,15 @@ void closeLog(void)
     saveLog("Plik log zamkniety");
     logFile.close();
 }
+
+uint8_t hammingDistance(uint8_t n1, uint8_t n2)
+{
+    uint8_t x = n1 ^ n2; // XOR
+    uint8_t setBits = 0;
+    while (x > 0)
+    {
+        setBits += x & 1;
+        x >>= 1;
+    }
+    return setBits;
+}
